510b0cfb5bced137cd56ac72791a92762ade78e3
Marco Brandizi <marco.brandizi@gmail.com>
2026-03-05 17:22:57 +0100

    refact: move PG creation and saving into its own module, refactor test utils

---
+++ tests/ketltest/utils.py
@@ -0,0 +1,23 @@
+import types
+import pytest
+from pyspark.sql import SparkSession
+
+
+def forward_spark_session_fixture ( spark_session: SparkSession, module: types.ModuleType ):
+	"""
+	Forwards `spark_session` to the current module (the one invoking this function) as a module-level
+	global variable, named `_spark_session`. 
+
+	This is ugly, but needed for classes that uses Spark at `@classmethod` level, eg, to initialise 
+	data for multiple instance methods.
+
+	This should be used in a test module this way:
+
+	```python
+	@pytest.fixture ( scope = "module", autouse = True )
+	# @pytest.mark.usefixtures ( "spark_session" ) # shouldn't be needed
+	def _forward_spark_session_fixture ( spark_session: SparkSession ):
+		forward_spark_session_fixture ( spark_session, module = sys.modules[ __name__ ] )
+	```
+	"""
+	setattr ( module, "_spark_session", spark_session )	
