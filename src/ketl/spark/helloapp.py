4f421d696e00d151e6eca0165e5022a8e0f66eb4
Marco Brandizi <marco.brandizi@gmail.com>
2026-05-21 13:58:52 +0100

    build: add Spark helloapp [ci skip]

---
+++ src/ketl/spark/helloapp.py
@@ -0,0 +1,36 @@
+# A simple Spark app, which can be used to test if a Spark cluster is working, via:
+#
+# <Create and activate a virtualenv with this project installed>
+# $ spark-submit --master spark://<master_host>:7077 -m ketl.spark.helloapp
+# 
+from pyspark.sql import SparkSession
+import pyspark.sql.functions as F
+
+def main ():
+	size = 1000
+	bin_size = 0.2
+	with SparkSession.builder.appName ( "HelloSpark" ).getOrCreate() as spark:
+    # Create a DF with the column 'value' with 'size' random values between 0 and 1.
+		df = spark.range ( size ).select ( F.rand().alias ( "value" ) )
+		# Create a histogram DF, with 'bin' and 'count' columns, using 'bin_size'
+		# 'bin' column ranging in bin_size, bin_size*2, ... 1
+		histogram_df = (
+			df
+			.withColumn ( 
+				"bin", 
+				F.round ( 
+					(F.floor ( df["value"] / bin_size ) + 1) * bin_size,
+					1
+				)
+			)
+			.groupBy("bin")
+			.count()
+			.orderBy("bin")
+		)
+
+		print ( "\n\n|=== Result:" )
+		histogram_df.show()
+		print ( "\nDone!\n" )
+    
+if __name__ == "__main__":
+    main()
--- src/scripts/spark-utils/slurm-server/kslurm-spark-start.sh
+++ src/scripts/spark-utils/slurm-server/kslurm-spark-start.sh
@@ -2,20 +2,22 @@
 set -euo pipefail
 
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+my_base_name="$(basename "${BASH_SOURCE[0]}" '.sh')"
 
 export SPARK_NODES=2
 export SPARK_CORES=4
 export SPARK_RAM=4
 export SPARK_TIME="02:00:00"
-export SPARK_TRACK_PATH=""
+export SPARK_TRACK_PATH="spark-server"
 export SPARK_PORT=7077
 export SPARK_WEB_PORT=8080
+sbatch_opts=""
 
 usage() {
   cat <<EOF
 
 
-Usage: $(basename "$0") [OPTIONS]
+Usage: ${my_base_name}.sh [OPTIONS]
 
 Start a Spark standalone cluster as a SLURM job.
 
@@ -31,6 +33,7 @@ Options:
                             <path>.master  — Spark master hostname
                             <path>.port    — Spark master port
                             <path>.jobid   — SLURM job ID that started the cluster (through this script)
+                          Default: $SPARK_TRACK_PATH
   --port <num>            Spark master port (default: $SPARK_PORT)
   --web-port <num>        Spark master web UI port (default: $SPARK_WEB_PORT)
   --sbatch <option>       <option> is passed to the 'sbatch' command (can override #SBATCH)
@@ -58,12 +61,13 @@ while [[ $# -gt 0 ]]; do
     --track)        SPARK_TRACK_PATH="$2";   shift 2 ;;
     --port)         SPARK_PORT="$2";         shift 2 ;;
     --web-port)     SPARK_WEB_PORT="$2";     shift 2 ;;
+    --sbatch)       sbatch_opts+=" $2";     shift 2 ;;
     --help)         usage; exit 0 ;;
     *) echo "Unknown option: $1" >&2; usage >&2; exit 1 ;;
   esac
 done
 
-sbatch_path="$script_dir/spark-start.sbatch"
+sbatch_path="$script_dir/${my_base_name}.sbatch"
 
 printf "\n\n|==== Starting the Spark cluster\n\n"
 
@@ -73,6 +77,7 @@ submit_out=$(sbatch \
   --cpus-per-task="$SPARK_CORES" \
   --mem="${SPARK_RAM}G" \
   --time="$SPARK_TIME" \
+  $sbatch_opts \
   "$sbatch_path" \
   | tee >(cat >&3) \
 )
