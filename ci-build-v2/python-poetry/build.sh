091a256b2108fe81a9c076931561b049f1bc8333
Marco Brandizi <marco.brandizi@gmail.com>
2026-06-03 17:25:10 +0100

    ci: add distro file rebuild to the CI workflow

--- ci-build-v2/python-poetry/build.sh
+++ ci-build-v2/python-poetry/build.sh
@@ -1,15 +1,34 @@
 #!/usr/bin/env bash
 set -eE -o pipefail
 
+# For the moment, we are publishing upon each commit, since we download distro files straight
+# from github, even if they aren't released yet. TODO: to be changed later.
+#
+function stage_build_local
+{
+	stage_build
+	poetry $CI_POETRY_DEFAULT_ARGS build
+
+  # Are there actual changes to commit?
+	[[ -z "$(git status --porcelain)" ]] && return 0
+	
+	git commit -m "build: upgrade the distro files on github (from CI script)"
+	export CI_NEEDS_PUSH=true
+}
+
+
+# At least for now, we override the default by removing PyPI publishing, since, for the time being,
+# we link this project through its github URL.
+# 
+# This will need to be changed in future, see stage_build_local() above.
+#
 function stage_release_local
 {
 	is_release_mode || return 0
 
-	# At least for now, we override the default by removing PyPI publishing, since, for the time being,
-	# we link this project through its github URL.
-
 	printf "== TODO: Skipping build+PyPI publishing for now, fix it when necessary\n"
-	poetry $CI_POETRY_DEFAULT_ARGS build
+	# TODO: see stage_build_local() for details on why this is not done here.
+	# poetry $CI_POETRY_DEFAULT_ARGS build
 
 	printf "== Moving the project to the next version\n"
 	poetry  $CI_POETRY_DEFAULT_ARGS version ${CI_NEW_SNAPSHOT_VER}
